9c269177b26defd79609bf9860d4553a81661ffc
kenick <zhanggw91@163.com>
2020-11-14 09:38:53 +0800

    适配手机端显示

--- src/main/resources/static/project-windows.js
+++ src/main/resources/static/project-windows.js
@@ -1,8 +1,9 @@
 $('#tab tr').each(function(i){
-    $(this).children('td').each(function(j){  // 遍历 tr 的各个 td
-        if(j==2){
-            console.log("第"+(i+1)+"行，第"+(j+1)+"个td的值："+$(this).text()+"。");
-            $(this).html(cutString($(this).text(),100));
+    // 折叠过长文字
+    $(this).children('th').each(function(j){  // 遍历 tr 的各个 td
+        if(j==1){
+            $(this).html(cutString($(this).text(),50));
+            $(this).css("width",300);
         }
     });
 });
