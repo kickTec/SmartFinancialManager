7c7c1ef73fcb24e963eef031ffb8913c233775a2
kickTec <zhanggw91@163.com>
2023-03-13 10:17:17 +0800

    增加SSL

--- src/main/java/com/kenick/Application.java
+++ src/main/java/com/kenick/Application.java
@@ -1,14 +1,32 @@
 package com.kenick;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
+	@Value("${server.http-port}")
+	private Integer httpPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		System.out.println("httpPort:"+httpPort);
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		tomcat.addAdditionalTomcatConnectors(connector);
+		return tomcat;
 	}
 
 }
